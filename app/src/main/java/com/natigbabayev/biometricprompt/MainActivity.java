b0beda3d4b549f270744fcd96a499bc7f0ecab32
natiginfo <natig.nizami@gmail.com>
2018-12-31 15:52:59 +0200

    feat: biometricprompt sample added

--- app/src/main/java/com/natigbabayev/biometricprompt/MainActivity.java
+++ app/src/main/java/com/natigbabayev/biometricprompt/MainActivity.java
@@ -1,7 +1,15 @@
 package com.natigbabayev.biometricprompt;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+
+        FragmentActivity activity = this;
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(activity, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                if (errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON) {
+                    // user clicked negative button
+                } else {
+                    //TODO: Called when an unrecoverable error has been encountered and the operation is complete.
+                }
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                //TODO: Called when a biometric is recognized.
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                //TODO: Called when a biometric is valid but not recognized.
+            }
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Set the title to display.")
+                .setSubtitle("Set the subtitle to display.")
+                .setDescription("Set the description to display")
+                .setNegativeButtonText("Negative Button")
+                .build();
+
+        findViewById(R.id.authenticateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
     }
 }
